cc84e0fbfc75a018350854aa26b1ef472d21dc2a
StepanTita <stepan.tytarenko@nure.ua>
2020-10-03 15:20:56 +0300

    remove swagger artifacts

--- goli/settings.py
+++ goli/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'authentication',
-    'rest_framework_swagger',
+    # 'rest_framework_swagger',
 ]
 
 REST_FRAMEWORK = {
