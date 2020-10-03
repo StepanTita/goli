ca06fa694a197afa623873e66ed376d517ba9034
StepanTita <stepan.tytarenko@nure.ua>
2020-10-03 15:09:51 +0300

    update requirements, remove Dockerfile

--- goli/settings.py
+++ goli/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -161,3 +162,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #         }
 #     },
 # }
+django_heroku.settings(locals())
