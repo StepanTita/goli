7b7e9b3362ca7c934e877668b339e1532e9e9d7c
StepanTita <stepan.tytarenko@nure.ua>
2020-10-04 11:58:05 +0300

    fix serializer return description + author

--- quizes/serializers.py
+++ quizes/serializers.py
@@ -11,7 +11,8 @@ logger = logging.getLogger(__name__)
 class QuizSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vote
-        fields = ['id', 'title', 'quiz_type', 'timestamp', 'goal', 'indicator_value', 'vote_detail']
+        fields = ['id', 'title', 'quiz_type', 'timestamp', 'goal', 'indicator_value', 'vote_detail', 'description',
+                  'author']
 
 
 class VoteChoiseSerializer(serializers.ModelSerializer):
