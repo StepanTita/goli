40c0b03fd6ce757c08600fd05e84eb2446269799
StepanTita <stepan.tytarenko@nure.ua>
2020-10-04 12:14:03 +0300

    fix password auth

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ from users import views
 urlpatterns = [
     path('groups/', views.GroupAPIView.as_view(), name='groups'),
     path('<str:username>/', views.UserAPIView.as_view(), name='user'),
-    path('groups/<str:name>', views.AddGroupUsersAPIView.as_view(), name='users_to_group')
+    path('groups/<str:name>/', views.AddGroupUsersAPIView.as_view(), name='users_to_group')
 ]
